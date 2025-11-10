fbf33018bf4eab5c0c680e7d757681af50540197
runner <runner@hmpps-github-actions-runner-8649b65f8f-zshjj>
2025-11-10 09:54:10 +0000

    Rename project

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accommodationdatadomainapi/AccommodationDataDomainApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accommodationdatadomainapi/AccommodationDataDomainApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.templatepackagename
+package uk.gov.justice.digital.hmpps.accommodationdatadomainapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class AccommodationDataDomainApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<AccommodationDataDomainApi>(*args)
 }
