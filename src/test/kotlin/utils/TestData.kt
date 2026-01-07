a59b4005afe98b5b3939383c1f2acc03b26cbde4
daveawc <168838504+daveawc@users.noreply.github.com>
2026-01-07 18:48:12 +0000

    adding detekt.
    Pinned java at 21 and used the lastsupportedkotlin function, until detekt 2.0.0 is released

---
+++ src/test/kotlin/utils/TestData.kt
@@ -0,0 +1,8 @@
+package utils
+
+/*
+ * This annotation can be used to exclude test data setup that would fail detekt,
+ * such as LongParameterList and LongFunction. This is set in detekt/detekt.yml
+ *
+ */
+internal annotation class TestData
