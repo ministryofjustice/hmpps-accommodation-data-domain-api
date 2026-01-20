da294c527354ad3344a9669bf51250285da506e8
daveawc <168838504+daveawc@users.noreply.github.com>
2026-01-20 11:56:12 +0000

    build.gradle updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,9 @@ plugins {
 }
 
 val hmppsSpringBootVersion = "2.0.0"
+val hmppsSqsVersion = "6.0.0"
 val springdocVersion = "3.0.1"
+val wiremockVersion = "3.13.2"
 
 dependencies {
   runtimeOnly("org.postgresql:postgresql")
@@ -16,9 +18,9 @@ dependencies {
   implementation("org.flywaydb:flyway-core")
 
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:$hmppsSpringBootVersion")
-  implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:$hmppsSqsVersion")
+
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.11")
   implementation("org.springframework.boot:spring-boot-restclient")
   implementation("net.javacrumbs.shedlock:shedlock-spring:5.8.0")
   implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:5.8.0")
@@ -28,7 +30,7 @@ dependencies {
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:$hmppsSpringBootVersion")
   testImplementation("org.springframework.boot:spring-boot-restclient-test")
 
-  testImplementation("org.wiremock:wiremock-standalone:3.13.2")
+  testImplementation("org.wiremock:wiremock-standalone:$wiremockVersion")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.37") {
     exclude(group = "io.swagger.core.v3")
