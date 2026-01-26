4a97d64165202d1bedd8874bdc2f3d5039dbd30b
daveawc <168838504+daveawc@users.noreply.github.com>
2026-01-26 14:04:02 +0000

    updated package for delius client

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accommodationdatadomainapi/infrastructure/client/delius/ProbationIntegrationDeliusClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accommodationdatadomainapi/infrastructure/client/delius/ProbationIntegrationDeliusClient.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.accommodationdatadomainapi.client
+package uk.gov.justice.digital.hmpps.accommodationdatadomainapi.infrastructure.client.delius
 
 import org.springframework.stereotype.Service
 import org.springframework.web.service.annotation.GetExchange
